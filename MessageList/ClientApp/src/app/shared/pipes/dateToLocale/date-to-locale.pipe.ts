4ebd8dd23bd2699d747c54a5b37da0da3883bd13
vladimircult <vladimircult@gmail.com>
2020-12-08 13:06:13 +0500

    Added exception guard to DateToLocale pipe An exception will be thrown if it is used not with string

--- MessageList/ClientApp/src/app/shared/pipes/dateToLocale/date-to-locale.pipe.ts
+++ MessageList/ClientApp/src/app/shared/pipes/dateToLocale/date-to-locale.pipe.ts
@@ -6,6 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class DateToLocalePipe implements PipeTransform {
   transform(value: string, toLocale: string, args?: any) {
+    if (typeof value !== 'string') {
+      throw new Error('DateToLocale can only be used with Date formatted strings');
+    }
     return new Date(value).toLocaleString(toLocale);
   }
 }
