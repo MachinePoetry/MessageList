7ac8e5eaab5575db3541ec18ea6463c93cc7ca29
vladimircult <vladimircult@gmail.com>
2021-01-03 19:15:51 +0500

    Changed returning sanitizer method in 'safeUrl' pipe

--- MessageList/ClientApp/src/app/shared/pipes/safeUrl/safe-url.pipe.ts
+++ MessageList/ClientApp/src/app/shared/pipes/safeUrl/safe-url.pipe.ts
@@ -9,6 +9,7 @@ export class SafeUrl implements PipeTransform {
   constructor(private _sanitizer: DomSanitizer) { }
 
   public transform(url: string) {
-    return this._sanitizer.bypassSecurityTrustUrl(url);
+    //return this._sanitizer.bypassSecurityTrustUrl(url);
+    return this._sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 }
