34aad4dd8257d25ff590244abca36b766db95e6d
vladimircult <vladimircult@gmail.com>
2021-01-10 17:30:19 +0500

    Added migration with 'IsGreeted' field for 'User' class

---
+++ MessageList/Migrations/20210110111805_UserGreeting.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MessageList.Migrations
+{
+    public partial class UserGreeting : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_greeted",
+                table: "users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_greeted",
+                table: "users");
+        }
+    }
+}
