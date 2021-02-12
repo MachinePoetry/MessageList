4ef828f6535d43c3d46e432bc961fc823a05f869
vladimircult <vladimircult@gmail.com>
2021-02-12 18:36:37 +0500

    Added EF migration with new table for Url previews

---
+++ MessageList/Migrations/20210211131341_AddedUrlPreviews.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace MessageList.Migrations
+{
+    public partial class AddedUrlPreviews : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "url_previews",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(type: "text", nullable: true),
+                    description = table.Column<string>(type: "text", nullable: true),
+                    image = table.Column<string>(type: "text", nullable: true),
+                    url = table.Column<string>(type: "text", nullable: true),
+                    related_message_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_url_previews", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_url_previews_messages_related_message_id",
+                        column: x => x.related_message_id,
+                        principalTable: "messages",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_url_previews_related_message_id",
+                table: "url_previews",
+                column: "related_message_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "url_previews");
+        }
+    }
+}
