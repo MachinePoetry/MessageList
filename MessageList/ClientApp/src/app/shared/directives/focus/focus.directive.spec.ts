7dc608771a0ed8320c3804023f42cf865c9aa593
vladimircult <vladimircult@gmail.com>
2021-03-11 18:45:16 +0500

    Fixed flacky Jasmine unit test for 'Focus' directive

--- MessageList/ClientApp/src/app/shared/directives/focus/focus.directive.spec.ts
+++ MessageList/ClientApp/src/app/shared/directives/focus/focus.directive.spec.ts
@@ -9,14 +9,15 @@ import { FocusDirective } from './focus.directive';
 export class TestFocusComponent { }
 
 describe('FocusDirective', () => {
-  let container, fixture;
-  beforeEach(() => {
+  let component, fixture;
+  beforeEach(async() => {
     TestBed.configureTestingModule({
       declarations: [TestFocusComponent, FocusDirective],
     });
     fixture = TestBed.createComponent(TestFocusComponent);
-    container = fixture.componentInstance;
+    component = fixture.componentInstance;
     fixture.detectChanges();
+    await fixture.whenStable();
   });
 
   it('should create the Focus directive', () => {
@@ -24,10 +25,15 @@ describe('FocusDirective', () => {
     expect(directive).toBeTruthy();
   });
 
+  // test below is flaky
+  //it('should give focus to parent component after component was created', () => {
+  //  let input: HTMLInputElement = fixture.nativeElement.querySelector('input');
+  //  let focusedInput: HTMLInputElement = fixture.nativeElement.querySelector(':focus');
+  //  fixture.detectChanges();
+  //  expect(focusedInput).toBe(input);
+  //});
+
   it('should give focus to parent component after component was created', () => {
-    let input: HTMLInputElement = fixture.nativeElement.querySelector('input');
-    let focusedInput: HTMLInputElement = fixture.nativeElement.querySelector(':focus');
-    fixture.detectChanges();
-    expect(focusedInput).toBe(input);
+    expect(document.activeElement.tagName).toBe('INPUT');
   });
 });
