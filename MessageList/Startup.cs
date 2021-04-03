76a1f21dd07c064dcdec468f8a6fc2c2305cd5dc
vladimircult <vladimircult@gmail.com>
2021-04-03 16:25:08 +0500

    Fix: removed .UseHsts() method to start application at prod

--- MessageList/Startup.cs
+++ MessageList/Startup.cs
@@ -57,7 +57,7 @@ namespace MessageList
                 app.UseExceptionHandler("/Home/Error");
             }
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
+            //app.UseHsts();
             app.UseHttpsRedirection();
             app.UseRouting();
             app.UseAuthentication();
